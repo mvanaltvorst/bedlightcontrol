9e49804bc2d754caa0182ecac611fe0c1f54e353
Maurits van Altvorst <mvanaltvorst@icloud.com>
2019-08-01 14:27:44 +0200

    Update WiFi credential management

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,24 @@
 #include <ESP8266WebServer.h>
 #include <FastLED.h>
 #include <vector>
-
+#include "config.h"
 
 #define PORT 80
 #define WIFI_RETRY_DELAY 500
 #define MAX_WIFI_INIT_RETRY 50
 
-#define NUM_LEDS 150
+#define NUM_LEDS 91
 #define DATA_PIN 5 // D1 
 
-#define WIFI_LED 5 // light on the led strip
-
-const char * WIFI_SSID = "Linden";
-const char * WIFI_PASS = "PASSWORD";
+#define WIFI_LED 20 // light on the led strip that will flicker to indicate WiFi status
 
 CRGB leds[NUM_LEDS];
 CRGB hiddenleds[NUM_LEDS];
-CRGB bgcolor;
+CRGB bgcolor = CRGB(0, 0, 0);
 
 bool isOn = false;
 
-
 ESP8266WebServer http_rest_server(PORT);
-
-// void log(const char * string) {
-//   Serial.println(string);
-// }
 
 void turnOff() {
   for (int i = 0; i < NUM_LEDS; i++) {
@@ -44,51 +36,78 @@ void turnOffService() {
 }
 
 void showLEDs() {
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = hiddenleds[i];
-  }
   FastLED.show();
   isOn = true;
 }
 
 void turnOnService() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = hiddenleds[i];
+  }
   showLEDs();
   http_rest_server.send(201);
 }
 
-void updateLeds() {
-  if (isOn) showLEDs();
+void flush() {
+  if (!isOn) return;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = hiddenleds[i];
+  }
+  showLEDs();
+  http_rest_server.send(201);
 }
 
+void updateIntermediate() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if      (leds[i].r > hiddenleds[i].r) leds[i].r--;
+    else if (leds[i].r < hiddenleds[i].r) leds[i].r++;
+
+    if      (leds[i].g > hiddenleds[i].g) leds[i].g--;
+    else if (leds[i].g < hiddenleds[i].g) leds[i].g++;
+
+    if      (leds[i].b > hiddenleds[i].b) leds[i].b--;
+    else if (leds[i].b < hiddenleds[i].b) leds[i].b++;
+  }
+  FastLED.show();
+}
+
+void gradient() {
+  if (!isOn) return;
+  for (int i = 0; i < 256; i++) {
+    updateIntermediate();
+  }
+  http_rest_server.send(201);
+}
 
 void set_led() {
-  Serial.println("Setting LED...");
+  // Serial.println("Setting LED...");
   hiddenleds[http_rest_server.arg("n").toInt()] = CRGB(
     http_rest_server.arg("r").toInt(), 
     http_rest_server.arg("g").toInt(), 
     http_rest_server.arg("b").toInt()
   );
-  updateLeds();
   http_rest_server.send(201);
 }
 
 void clear_led() {
   hiddenleds[http_rest_server.arg("n").toInt()] = bgcolor;
-  updateLeds();
   http_rest_server.send(201);
 }
 
 void set_bg_color() {
+  // Serial.println("Setting bg color");
+  // Serial.printf("bgcolor: %d %d %d\n", bgcolor.r, bgcolor.g, bgcolor.b);
   CRGB newbgcolor = CRGB(
     http_rest_server.arg("r").toInt(), 
     http_rest_server.arg("g").toInt(), 
     http_rest_server.arg("b").toInt()
   );
-  for (int i = 0; i < 150; i++) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    // Serial.printf("Old color: %d %d %d\n", hiddenleds[i].r, hiddenleds[i].g, hiddenleds[i].b);
     if (hiddenleds[i] = bgcolor) hiddenleds[i] = newbgcolor;
   }
   bgcolor = newbgcolor;
-  updateLeds();
+  // Serial.printf("New bg color: %d %d %d\n", newbgcolor.r, newbgcolor.g, newbgcolor.b);
   http_rest_server.send(201);
 }
 
@@ -100,20 +119,22 @@ void set_range() {
   );
   int nStart = http_rest_server.arg("ns").toInt();
   int nEnd   = http_rest_server.arg("ne").toInt();
-  for (int i = nStart; i < nEnd; i++) {
+  for (int i = nStart; i <= nEnd; i++) {
     hiddenleds[i] = newcolor;
   }
-  updateLeds();
   http_rest_server.send(201);
 }
 
 void clear_range() {
+  // Serial.print("Clearing range ");
   int nStart = http_rest_server.arg("ns").toInt();
   int nEnd   = http_rest_server.arg("ne").toInt();
-  for (int i = nStart; i < nEnd; i++) {
+  // Serial.print(nStart);
+  // Serial.println(nEnd);
+  for (int i = nStart; i <= nEnd; i++) {
     hiddenleds[i] = bgcolor;
+    // Serial.printf("Setting %d to bgcolor\n", i);
   }
-  updateLeds();
   http_rest_server.send(201);
 }
 
@@ -123,10 +144,11 @@ void setup() {
   delay(1000);
   Serial.begin(9600);
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  for (int i = 0; i < NUM_LEDS; i++) hiddenleds[i] = CRGB(0, 0, 0);
 
   int retries = 0;
   WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println("Connecting to WiFi...");
   while ((WiFi.status() != WL_CONNECTED) && (retries < MAX_WIFI_INIT_RETRY)) {
     retries++;
@@ -165,77 +187,19 @@ void setup() {
   http_rest_server.on("/", HTTP_GET, []() {
     http_rest_server.send(200, "text/html", "HTTP works!");
   });
-  http_rest_server.on("/setLed", HTTP_POST, set_led);
-  http_rest_server.on("/clearLed", HTTP_POST, clear_led);
-  http_rest_server.on("/setBgColor", HTTP_POST, set_bg_color);
-  http_rest_server.on("/turnOn", HTTP_POST, turnOnService);
-  http_rest_server.on("/turnOff", HTTP_POST, turnOffService);
-  http_rest_server.on("/setRange", HTTP_POST, set_range);
-  http_rest_server.on("/clearRange", HTTP_POST, clear_range);
+  http_rest_server.on("/setLed", HTTP_GET, set_led);
+  http_rest_server.on("/clearLed", HTTP_GET, clear_led);
+  http_rest_server.on("/setBgColor", HTTP_GET, set_bg_color);
+  http_rest_server.on("/turnOn", HTTP_GET, turnOnService);
+  http_rest_server.on("/turnOff", HTTP_GET, turnOffService);
+  http_rest_server.on("/setRange", HTTP_GET, set_range);
+  http_rest_server.on("/clearRange", HTTP_GET, clear_range);
+  http_rest_server.on("/flush", HTTP_GET, flush);
+  http_rest_server.on("/gradient", HTTP_GET, gradient);
   http_rest_server.begin();
 }
 
-// CRGB HSVtoRGB(int h, double s, double v) {
-//   //this is the algorithm to convert from RGB to HSV
-//   double r=0; 
-//   double g=0; 
-//   double b=0;
-
-//   double hf=h/60.0;
-
-//   int i=(int)floor(h/60.0);
-//   double f = h/60.0 - i;
-//   double pv = v * (1 - s);
-//   double qv = v * (1 - s*f);
-//   double tv = v * (1 - s * (1 - f));
-
-//   switch (i)
-//   {
-//   case 0: //rojo dominante
-//     r = v;
-//     g = tv;
-//     b = pv;
-//     break;
-//   case 1: //verde
-//     r = qv;
-//     g = v;
-//     b = pv;
-//     break;
-//   case 2: 
-//     r = pv;
-//     g = v;
-//     b = tv;
-//     break;
-//   case 3: //azul
-//     r = pv;
-//     g = qv;
-//     b = v;
-//     break;
-//   case 4:
-//     r = tv;
-//     g = pv;
-//     b = v;
-//     break;
-//   case 5: //rojo
-//     r = v;
-//     g = pv;
-//     b = qv;
-//     break;
-//   }
-//   return CRGB(r * 255, g * 255, b * 255);
-// }
-
-// int i = 0;
-
 void loop() {
-  // put your main code here, to run repeatedly:
   http_rest_server.handleClient();
   ArduinoOTA.handle();
-
-  // leds[0] = HSVtoRGB(i, 1, 1);
-  // for (int i = NUM_LEDS - 1; i > 0; i--) {
-  //   leds[i] = leds[i-1];
-  // }
-  // FastLED.show();
-  // i = (i + 1) % 360;
 }
